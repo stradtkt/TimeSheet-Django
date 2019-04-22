1299934797d0b6f9480361f35cf2d0f7b2ab4a96
Kevin <stradtkt22@gmail.com>
2019-04-22 00:06:05 -0400

    Added login and register functionality

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,11 +3,58 @@ from django.core.exceptions import ValidationError
 from django.db import models
 
 class UserManager(models.Manager):
-	pass
+	def validate_user(self, postData):
+		errors = {}
+		if len(postData['first_name']) < 2:
+			errors['first_name'] = "First name needs to be 2 or more characters"
+		if len(postData['last_name']) < 2:
+			errors['last_name'] = "Last name needs to be 2 or more characters"
+		if len(postData['first_name']) > 30:
+			errors['first_name'] = "First name needs to be less then 30 characters"
+		if len(postData['last_name']) > 30:
+			errors['last_name'] = "Last name needs to be less then 30 characters"
+		if not postData['first_name'].isalpha():
+			errors['first_name'] = "First name needs to be only letters"
+		if not postData['last_name'].isalpha():
+			errors['last_name'] = "Last name needs to be only letters"
+
+		try:
+			validate_email(postData['email'])
+		except ValidationError:
+			errors['email'] = "Your email is not valid"
+		else:
+			if User.objects.filter(email=postData['email']):
+				errors['email'] = "This email already exists"
+
+		try:
+			postData['username']
+		except ValidationError:
+			errors['username'] = "Your username is not valid"
+		else:
+			if User.objects.filter(username=postData['username']):
+				errors['username'] = "This username already exists"
+
+		if len(postData['password']) < 4:
+			errors['password'] = "Please enter a longer password, needs to be four or more characters"
+		if postData['password'] != postData['confirm_pass']:
+			errors['confirm_pass'] = "Passwords must match"
+		return errors
+
 
 class User(models.Model):
 	first_name = models.CharField(max_length=255)
 	last_name = models.CharField(max_length=255)
 	email = models.EmailField()
 	password = models.CharField(max_length=255)
-	objects.UserManager()
+	created_at = models.DateTimeField(auto_now_add=True)
+	objects = UserManager()
+
+class Points(models.Model):
+	user = models.ForeignKey(User, verbose_name="user_points")
+	points = models.IntegerField(default=0)
+
+class DailyReport(models.Model):
+	today = models.TextField()
+	challenges = models.TextField()
+	need_help = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.contrib import messages
+from .models import *
+import bcrypt
 
-# Create your views here.
+
 def index(request):
 	return render(request, "dashboard/index.html")
+
+def login(request):
+    email = request.POST['email']
+    password = request.POST['password']
+    user = User.objects.filter(email=email)
+    if len(user) > 0:
+        is_pass = bcrypt.checkpw(password.encode('utf-8'), user[0].password.encode('utf-8'))
+        if is_pass:
+            request.session['id'] = user[0].id
+            return redirect('/dashboard')
+        else:
+            messages.error(request, "Incorrect email and/or password")
+            return redirect('/')
+    else:
+        messages.error(request, "User does not exist")
+    return redirect('/')
+
+def register(request):
+    errors = User.objects.validate_user(request.POST)
+    if len(errors):
+        for tag, error in errors.items():
+            messages.error(request, error)
+        return redirect('/')
+    else:
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        username = request.POST['username']
+        location = request.POST['location']
+        password = request.POST['password']
+        hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        User.objects.create(first_name=first_name, last_name=last_name, location=location, username=username, email=email, password=hashed_pw)
+        return redirect('/')
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
